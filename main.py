326a8e3330cb5bb4d4defc6a48b4bddfa1cf04ec
danyanderson2 <danyanderson2222@gmail.com>
2024-07-22 16:28:39 +0100

    update to main.py change

--- main.py
+++ main.py
@@ -2,22 +2,49 @@ from moviepy.editor import VideoFileClip
 import os
 
 
-def convert_mp4_to_mp3(mp4_file, mp3_file):
+
+# Hi!!! No need to go beneath the area below if you're a nove at coding ;)
+######################################################################################################################
+PATH_TO_FOLDER_WITH_VIDEO_FILES = ''    # path to input folder
+PATH_TO_OUTPUT_FOLDER =r"D:\folder"              # path to output folder
+
+#######################################################################################################################
+# in case you have n files in a folder which you want to convert all to audio with single
+
+
+def convert_video_to_mp3(video_path, output_folder):
+    # Check if the video file exists
+    if not os.path.isfile(video_path):
+        print(f"The file {video_path} does not exist.")
+        return
+
+    # Extract the file name without extension
+    file_name = os.path.splitext(os.path.basename(video_path))[0]
+
     # Load the video file
-    video_clip = VideoFileClip(mp4_file)
+    video_clip = VideoFileClip(video_path) # instantiate a VideoFileClip object
 
-    # Extract the audio
-    audio_clip = video_clip.audio
+    # Construct the output file path
+    output_file_path = os.path.join(output_folder, f"{file_name}.mp3")
 
-    # Write the audio to an mp3 file
-    audio_clip.write_audiofile(mp3_file, codec='mp3')
+    # Write the audio to an MP3 file
+    video_clip.audio.write_audiofile(output_file_path)  # actual conversion
+    print(f"Converted {video_path} to {output_file_path}")
 
-    # Close the clips
-    audio_clip.close()
-    video_clip.close()
+# Calling function
+video_formats = ['avi', 'mp4', 'mkv', 'mov', 'mpeg', 'flv', 'wmv']
 
+# Create the output folder if it doesn't exist
+os.makedirs(PATH_TO_OUTPUT_FOLDER, exist_ok=True)
 
-for i in range(1,9):
-    convert_mp4_to_mp3(f'D:\Le Coffret VIP\Avant le lancement\mois pour devenir mon propre patron\Vidéos\Module {i}.mp4',
-                       f'D:\Le Coffret VIP\Avant le lancement\mois pour devenir mon propre patron\Vidéos\Module {i}.mp3')
-
+# Loop through the video files in the input folder and convert them
+try:
+    os.listdir(PATH_TO_FOLDER_WITH_VIDEO_FILES)
+except FileNotFoundError:
+    print('File not found')
+else:
+    for file_name in os.listdir(PATH_TO_FOLDER_WITH_VIDEO_FILES):
+        if file_name.split('.')[-1].lower() in video_formats:
+            video_path = os.path.join(PATH_TO_FOLDER_WITH_VIDEO_FILES, file_name)
+            convert_video_to_mp3(video_path, PATH_TO_OUTPUT_FOLDER)
+# single file converters
